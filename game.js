0e81e5259a6710b77e9adcb7eb39747c85c79ba8
Keith Grant <keithjgrant@gmail.com>
2012-02-12 14:03:57 -0500

    Begin level loader

--- game.js
+++ game.js
@@ -18,10 +18,9 @@ SKB.conf = {
 SKB.init = function() {
     var conf = SKB.conf;
     Crafty.init([conf.GAME_WIDTH * conf.TILE, conf.GAME_HEIGHT * conf.TILE]);
-
     Crafty.sprite(conf.TILE, 'sprites.png', {
         player: [0, 0],
-        tile: [0, 1],
+        wall: [0, 1],
         darkBlock: [0, 2],
         lightBlock: [0, 3]
     });
@@ -29,30 +28,64 @@ SKB.init = function() {
 
 SKB.loadLevel = function(level) {
     var name = 'level' + level;
-console.log(Crafty._scenes);
+
     if (Crafty._scenes[name]) {
         // already loaded
         Crafty.scene(name);
         return;
     }
 
-    $.get(name + '.json', function(data) {
-        console.log(data);
-        for (r=0; r<SKB.conf.GAME_HEIGHT; r++) {
-            for (c=0; c<SKB.conf.GAME_WIDTH; c++) {
-                if (data[r] && data[r][c]) {
-                    SKB.loadTile(data[r][c]);
-                } else {
-                    SKB.loadTile('');
+    Crafty.scene(name, function() {
+        $.get(name + '.json', function(data) {
+            for (r = 0; r < SKB.conf.GAME_HEIGHT; r++) {
+                for (c = 0; c < SKB.conf.GAME_WIDTH; c++) {
+                    if (data[r] && data[r][c]) {
+                        SKB.loadTile(r, c, data[r][c]);
+                    } else {
+                        SKB.loadTile(r, c, 'x');
+                    }
                 }
             }
-        }
-                
+                    
+        }, 'json')
+        .error(function() {
+            if (!console || !console.log) { return; }
+            console.log('Error loading level: ' + name + '.json');
+        });
     });
+    Crafty.scene(name);
 };
 
-SKB.loadTile = function(data) {
-    console.log(data);
+SKB.loadTile = function(r, c, data) {
+    for (i = 0; i < data.length; i++) {
+        var char = data.charAt(i);
+        if (char === 'x') {
+            SKB.wallEntity(r, c);
+        } else if (char === 'p') {
+            SKB.playerEntity(r, c);
+        }
+    }
+};
+
+SKB.wallEntity = function(r, c) {
+    var wall = Crafty.e();
+    wall.addComponent("2D, DOM, wall");
+    wall.attr(SKB.attributes(r, c));
+};
+
+SKB.playerEntity = function(r, c) {
+    return Crafty.e("2D, DOM, player, controls, collision, fourwaysnap")
+                 .attr(SKB.attributes(r, c))
+                 .fourwaysnap(4, SKB.conf.TILE);
+};
+
+SKB.attributes = function(r, c) {
+    return {
+        x: c * SKB.conf.TILE,
+        y: r * SKB.conf.TILE,
+        w: SKB.conf.TILE,
+        h: SKB.conf.TILE
+    }
 };
 
 window.onload = function() {
