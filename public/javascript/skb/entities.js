4c5abe92eb348439eace56fe18efcf49c615f1e9
Keith Grant <kgrant@pictage.com>
2012-02-19 21:35:49 -0500

    implement block pushing

--- public/javascript/skb/entities.js
+++ public/javascript/skb/entities.js
@@ -11,18 +11,30 @@ SKB.entityLoader.prototype = {
 
     block: function(c, r) {
         return Crafty.e("2D, DOM, block, fourwaysnap")
-            .attr(this._attributes(c, r));
+            .attr(this._attributes(c, r))
+            .fourwaysnap(4, SKB.conf.TILE);
     },
 
     player: function(c, r) {
         var p = Crafty.e("2D, DOM, player, PlayerControls, Collision, fourwaysnap")
             .attr(this._attributes(c, r))
             .fourwaysnap(4, SKB.conf.TILE);
+
         p.collision()
          .onHit('wall', function() {
             this.stop();
         }).onHit('block', function(objects) {
             var block = objects[0].obj;
+
+            if (this._motion === 1) {
+                block.moveUp();
+            } else if (this._motion === 2) {
+                block.moveRight();
+            } else if (this._motion === 3) {
+                block.moveDown();
+            } else if (this._motion === 4) {
+                block.moveLeft();
+            }
         });
 
         return p;
