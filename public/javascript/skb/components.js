09d3e19f4842bb70fe95418fdc2d3c3298c53af3
Keith Grant <kgrant@pictage.com>
2012-02-19 21:20:37 -0500

    Separate player controls out from fourwaysnap component

--- public/javascript/skb/components.js
+++ public/javascript/skb/components.js
@@ -6,7 +6,6 @@
  * of the given gridSize.
  */
 Crafty.c("fourwaysnap", {
-    __move: {left: false, right: false, up: false, down: false},
     _speed: 4,
     _gridSize: 32,
     _motion: 0, // 0 = stopped; 1-4 = up, right, down, left
@@ -29,14 +28,13 @@ Crafty.c("fourwaysnap", {
             this._gridSize = gridSize;
             this._ticksPerSnap = gridSize / this._speed;
         }
-        var move = this.__move;
 
 
         this.bind("EnterFrame", function() {
             var old = this.pos(),
                 changed = false;
-
             if (this._motion && this._ticksRemaining === 1) {
+                // final frame -- don't step past landing tile
                 this.x = this._nextSnapPos.x,
                 this.y = this._nextSnapPos.y
                 this._ticksRemaining = 0;
@@ -66,53 +64,6 @@ Crafty.c("fourwaysnap", {
                 this._ticksRemaining--;
             }
 
-            if(move.up) {
-                this.moveUp();
-            }
-            if(move.right) {
-                this.moveRight();
-            }
-            if(move.down) {
-                this.moveDown();
-            }
-            if(move.left) {
-                this.moveLeft();
-            }
-
-        }).bind("KeyDown", function(e) {
-            if (e.keyCode === Crafty.keys.RIGHT_ARROW ||
-              e.keyCode === Crafty.keys.D) {
-                move.right = true;
-            }
-            if (e.keyCode === Crafty.keys.LEFT_ARROW ||
-              e.keyCode === Crafty.keys.A) {
-                move.left = true;
-            }
-            if (e.keyCode === Crafty.keys.UP_ARROW ||
-              e.keyCode === Crafty.keys.W) {
-                move.up = true;
-            }
-            if (e.keyCode === Crafty.keys.DOWN_ARROW ||
-              e.keyCode === Crafty.keys.S) {
-                move.down = true;
-            }
-        }).bind("KeyUp", function(e) {
-            if (e.keyCode === Crafty.keys.RIGHT_ARROW ||
-              e.keyCode === Crafty.keys.D) {
-                move.right = false;
-            }
-            if (e.keyCode === Crafty.keys.LEFT_ARROW ||
-              e.keyCode === Crafty.keys.A) {
-                move.left = false;
-            }
-            if (e.keyCode === Crafty.keys.UP_ARROW ||
-              e.keyCode === Crafty.keys.W) {
-                move.up = false;
-            }
-            if (e.keyCode === Crafty.keys.DOWN_ARROW ||
-              e.keyCode === Crafty.keys.S) {
-                move.down = false;
-            }
         });
 
         return this;
@@ -163,6 +114,7 @@ Crafty.c("fourwaysnap", {
 });
 
 Crafty.c("PlayerControls", {
+    __move: {left: false, right: false, up: false, down: false},
 
     init: function() {
         if (!this.has("Collision")) {
@@ -171,8 +123,56 @@ Crafty.c("PlayerControls", {
         if (!this.has('fourwaysnap')) {
             this.addComponent('fourwaysnap');
         }
+        var move = this.__move;
         
         this.bind("EnterFrame", function() {
+            // move entity based on active input
+            if(move.up) {
+                this.moveUp();
+            }
+            if(move.right) {
+                this.moveRight();
+            }
+            if(move.down) {
+                this.moveDown();
+            }
+            if(move.left) {
+                this.moveLeft();
+            }
+        }).bind("KeyDown", function(e) {
+            if (e.keyCode === Crafty.keys.RIGHT_ARROW ||
+              e.keyCode === Crafty.keys.D) {
+                move.right = true;
+            }
+            if (e.keyCode === Crafty.keys.LEFT_ARROW ||
+              e.keyCode === Crafty.keys.A) {
+                move.left = true;
+            }
+            if (e.keyCode === Crafty.keys.UP_ARROW ||
+              e.keyCode === Crafty.keys.W) {
+                move.up = true;
+            }
+            if (e.keyCode === Crafty.keys.DOWN_ARROW ||
+              e.keyCode === Crafty.keys.S) {
+                move.down = true;
+            }
+        }).bind("KeyUp", function(e) {
+            if (e.keyCode === Crafty.keys.RIGHT_ARROW ||
+              e.keyCode === Crafty.keys.D) {
+                move.right = false;
+            }
+            if (e.keyCode === Crafty.keys.LEFT_ARROW ||
+              e.keyCode === Crafty.keys.A) {
+                move.left = false;
+            }
+            if (e.keyCode === Crafty.keys.UP_ARROW ||
+              e.keyCode === Crafty.keys.W) {
+                move.up = false;
+            }
+            if (e.keyCode === Crafty.keys.DOWN_ARROW ||
+              e.keyCode === Crafty.keys.S) {
+                move.down = false;
+            }
         });
     }
     
